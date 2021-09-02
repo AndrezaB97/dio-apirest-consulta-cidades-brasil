174495ea26dd09a4817f57335b3a92eff0a5b930
AndrezaB97 <andreza.adsb@gmail.com>
2021-09-01 23:13:09 -0300

    get country by id

--- src/main/java/com/githubandrezabatista/citiesapi/resources/CountryResource.java
+++ src/main/java/com/githubandrezabatista/citiesapi/resources/CountryResource.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -19,6 +22,12 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page) {
         return countryRepository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public Country getOne(@PathVariable Long id) {
+        Optional<Country> optional = countryRepository.findById(id);
+        return optional.get();
     }
 
 }
